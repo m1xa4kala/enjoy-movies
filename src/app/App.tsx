8c4b1eabfbc4e07d08efb17acc4f2f08d2c18459
m1xa4kala <maikl2020011@gmail.com>
2022-06-14 15:34:00 +0300

    initial commit, create project and setup linter with formatter

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign='center' fontSize='xl'>
+      <Grid minH='100vh' p={3}>
+        <ColorModeSwitcher justifySelf='flex-end' />
+        <VStack spacing={8}></VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
