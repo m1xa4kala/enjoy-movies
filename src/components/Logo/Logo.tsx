e294530524b5a839ae4b71308399300ab60290eb
m1xa4kala <maikl2020011@gmail.com>
2022-06-15 22:43:36 +0300

    change default logo props sizes

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image, ImageProps } from '@chakra-ui/react'
 
-const Logo: React.FC<ImageProps> = ({ width = 30, height = 30 }) => {
+const Logo: React.FC<ImageProps> = ({ width = 7, height = 6 }) => {
   return <Image width={width} height={height} src='logo192.png' />
 }
 
