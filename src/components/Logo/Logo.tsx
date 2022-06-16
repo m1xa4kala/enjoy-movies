cd847fdf22b7566eb7310036c1a44460b1c65f43
m1xa4kala <maikl2020011@gmail.com>
2022-06-16 14:42:07 +0300

    animate logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Image, ImageProps } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 
 const Logo: React.FC<ImageProps> = ({ width = 7, height = 6 }) => {
-  return <Image width={width} height={height} src='logo192.png' />
+  return (
+    <motion.div whileHover={{scale: 1.1, rotate: 5}}>
+      <Image width={width} height={height} src='logo192.png' />
+    </motion.div>
+  )
 }
 
 export default Logo
