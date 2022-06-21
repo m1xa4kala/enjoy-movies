9ef2b73b2cff8df3434c6fb8cc879c35a7cb73bd
m1xa4kala <maikl2020011@gmail.com>
2022-06-21 15:09:11 +0300

    change Header sizes and container breackpoints

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,54 +8,56 @@ import {
   Link,
   useColorModeValue,
   useDisclosure,
+  chakra,
 } from '@chakra-ui/react'
-import Logo from '../Logo/Logo'
-import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { Link as RLink } from 'react-router-dom'
-import Search from '../Search/Search'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+import Search from '../Search/Search'
+import Logo from '../Logo/Logo'
 import Menu from './Menu/Menu'
+
+const Navbar = chakra('header')
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <header>
-      <Flex
-        py={1}
-        height='10'
-        bg={useColorModeValue('raisinBlack', 'whiteAlpha.200')}
-        alignItems='center'
-      >
-        <Container maxW={['sm', 'md', 'container.sm', 'container.md']}>
-          <Flex w='full' alignItems='center'>
-            <Link as={RLink} mr='1.5' to='/'>
-              <Logo width={8} />
-            </Link>
-            <Button
-              onClick={onOpen}
-              mr='1.5'
-              size='xs'
-              fontSize='xs'
-              aria-label='menu'
-              variant='ghost'
-              _hover={{ bg: 'whiteAlpha.200' }}
-              color='eggshell'
-              leftIcon={<HamburgerIcon fontSize='md' color='deepLemon' />}
-            >
-              Menu
-            </Button>
-            <Menu isOpen={isOpen} onClose={onClose}>
-              <DrawerOverlay />
-            </Menu>
-            <Box mr='1.5' w='full'>
-              <Search />
-            </Box>
-            <ColorModeSwitcher />
-          </Flex>
-        </Container>
-      </Flex>
-    </header>
+    <Navbar
+      bg={useColorModeValue('raisinBlack', 'whiteAlpha.200')}
+      py={1}
+      height='56px'
+      display='flex'
+      alignItems='center'
+    >
+      <Container maxW={['sm', 'container.sm', 'container.md', 'container.lg']}>
+        <Flex w='full' alignItems='center'>
+          <Link display={['none', 'block']} as={RLink} mr='1.5' to='/'>
+            <Logo width={10} height={8} />
+          </Link>
+          <Button
+            onClick={onOpen}
+            mr='1.5'
+            size='md'
+            fontSize='md'
+            aria-label='menu'
+            variant='ghost'
+            _hover={{ bg: 'whiteAlpha.200' }}
+            color='eggshell'
+            leftIcon={<HamburgerIcon mt='-0.5' fontSize='lg' color='deepLemon' />}
+          >
+            Menu
+          </Button>
+          <Menu isOpen={isOpen} onClose={onClose}>
+            <DrawerOverlay />
+          </Menu>
+          <Box mr='1.5' w='full'>
+            <Search />
+          </Box>
+          <ColorModeSwitcher />
+        </Flex>
+      </Container>
+    </Navbar>
   )
 }
 
